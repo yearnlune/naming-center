972d9901b5d524c9d614feb78d02e8eceb167899
DongHwan Kim <kdhpopoya@gmail.com>
2020-02-04 20:48:25 +0900

    init commit

---
+++ src/main/java/yearnlune/lab/namingcenter/NamingCenterApplication.java
@@ -0,0 +1,13 @@
+package yearnlune.lab.namingcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamingCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yearnlune/lab/namingcenter/NamingCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package yearnlune.lab.namingcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
