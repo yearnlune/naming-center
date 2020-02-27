07c0961ce61f7c04c8773c8d2dacebc52ea2ac3f
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-02-27 19:15:43 +0900

    add AccountCustomRepositoryImpl

---
+++ src/main/java/yearnlune/lab/namingcenter/database/repository/custom/impl/AccountCustomRepositoryImpl.java
@@ -0,0 +1,6 @@
+package yearnlune.lab.namingcenter.database.repository.custom.impl;
+
+import yearnlune.lab.namingcenter.database.repository.custom.AccountCustomRepository;
+
+public class AccountCustomRepositoryImpl implements AccountCustomRepository {
+}
