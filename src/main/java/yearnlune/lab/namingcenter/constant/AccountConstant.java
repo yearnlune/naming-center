3edf93cff0487cfd9829e58426d341b6f71863ad
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-02-28 13:42:05 +0900

    add LOGIN to AccountConstant

--- src/main/java/yearnlune/lab/namingcenter/constant/AccountConstant.java
+++ src/main/java/yearnlune/lab/namingcenter/constant/AccountConstant.java
@@ -2,5 +2,5 @@ package yearnlune.lab.namingcenter.constant;
 
 public class AccountConstant {
     public static final String ACCOUNT = "/accounts";
-
+    public static final String LOGIN = "/login";
 }
