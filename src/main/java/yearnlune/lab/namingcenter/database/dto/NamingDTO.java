fe050c8de0c481a25d9b2116943e04f1fd5f84ea
DongHwan Kim <kdhpopoya@gmail.com>
2020-03-03 18:44:27 +0900

    Add NamingDTO

---
+++ src/main/java/yearnlune/lab/namingcenter/database/dto/NamingDTO.java
@@ -0,0 +1,54 @@
+package yearnlune.lab.namingcenter.database.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yearnlune.lab.namingcenter.database.table.Account;
+
+import javax.persistence.Column;
+
+/**
+ * Project : naming-center
+ * Created by IntelliJ IDEA
+ * Author : DONGHWAN, KIM
+ * DATE : 2020.03.03
+ * DESCRIPTION :
+ */
+
+public class NamingDTO {
+
+    @Data
+    @NoArgsConstructor
+    public static class CommonResponse {
+        private Integer idx;
+
+        private String name;
+
+        private String description;
+
+        @Builder
+        public CommonResponse(Integer idx, String name, String description) {
+            this.idx = idx;
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class RegisterRequest {
+        private String name;
+
+        private String description;
+
+        private AccountDTO.CommonResponse account;
+
+        @Builder
+        public RegisterRequest(String name, String description, AccountDTO.CommonResponse account) {
+            this.name = name;
+            this.description = description;
+            this.account = account;
+        }
+    }
+
+}
