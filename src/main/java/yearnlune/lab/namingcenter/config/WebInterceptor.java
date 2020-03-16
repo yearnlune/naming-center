5447a7d94337dde51c77c352a4ffc5b8038c1fb2
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-03-16 16:12:10 +0900

    add interceptor & MVCconfig

---
+++ src/main/java/yearnlune/lab/namingcenter/config/WebInterceptor.java
@@ -0,0 +1,26 @@
+package yearnlune.lab.namingcenter.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Project : naming-center
+ * Created by IntelliJ IDEA
+ * Author : DONGHWAN, KIM
+ * DATE : 2020.03.16
+ * DESCRIPTION :
+ */
+
+@Slf4j
+public class WebInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+}
---
+++ src/main/java/yearnlune/lab/namingcenter/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package yearnlune.lab.namingcenter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Project : naming-center
+ * Created by IntelliJ IDEA
+ * Author : DONGHWAN, KIM
+ * DATE : 2020.03.16
+ * DESCRIPTION :
+ */
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WebInterceptor());
+    }
+}
