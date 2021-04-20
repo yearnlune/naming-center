df86ea0fc6e04e535faa918d2528fa8653ad0616
DONGHWAN, KIM <kdhpopoya@gmail.com>
2021-04-20 15:45:04 +0900

    feat: Added EnumMapperType

---
+++ src/main/java/yearnlune/lab/namingcenter/type/mapper/EnumMapperType.java
@@ -0,0 +1,16 @@
+package yearnlune.lab.namingcenter.type.mapper;
+
+import yearnlune.lab.namingcenter.type.AccountRoleType;
+
+/**
+ * Project : naming-center
+ * Created by IntelliJ IDEA
+ * Author : DONGHWAN, KIM
+ * DATE : 2021.04.19
+ * DESCRIPTION :
+ */
+public interface EnumMapperType {
+	String getCode();
+
+	String getValue();
+}
